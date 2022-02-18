9b39eca8a4bd9dbee58985edbc8d89b1b475beae
JOY <joybosu@gmail.com>
2022-02-19 00:39:27 +0530

    Welcome to flip coin

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace FlipProb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            FlipProb.AddTwoNumber();
+        }
+    }
+}
