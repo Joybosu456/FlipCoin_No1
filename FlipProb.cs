46c7c0b8d1940a07ff2cc245ab6674ec1412434f
JOY <joybosu@gmail.com>
2022-02-19 00:50:29 +0530

    add flip coin program ligic

---
+++ FlipProb.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlipProb
+{
+    internal class FlipProb
+    {
+        public static void FlipThecoin()
+        {
+            Console.WriteLine("Please enter the value of number of flips :");
+
+            int Head = 0;
+            int Tail = 0;
+            int HeadCount = 0;
+            int TailCount = 0;
+
+            double HeadPercentage;
+            double TailPercentage;  
+            int FlipProb=Convert.ToInt32(Console.ReadLine());
+
+            if(FlipProb>0)
+            {
+                for(int i=0;i<FlipProb; i++)
+                {
+                    Random random = new Random();
+                    int number = random.Next(2);
+                    if(number == 1)
+                    {
+                        Head = 1;
+                        HeadCount += Head;
+                    }
+                    else if(number == 0)
+                    {
+                        Tail = 1;
+                        TailCount+=Tail;
+                        Console.WriteLine(number);
+                    }
+                    Console.WriteLine("......................");
+
+                    HeadPercentage = HeadCount * 100 / FlipProb;
+                    TailPercentage = TailCount * 100 / FlipProb;
+
+                    Console.WriteLine("Head Percentage :" + HeadPercentage);
+                    Console.WriteLine("Tail percentage :" + TailPercentage);
+
+                }
+                
+            }
+            else
+            {
+                Console.WriteLine("invalid input");
+            }
+        }
+    }
+}
