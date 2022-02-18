be563cfb3a330d2a091380ee43b700b86e763ed5
JOY <joybosu@gmail.com>
2022-02-19 00:52:04 +0530

    harmic program

---
+++ Harmic.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlipProb
+{
+    public class Harmic
+    {
+        public static void HarmicNumber()
+        {
+            int count, num;
+            double sum = 0;
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Calculate the sum of harmonic series :\n");
+            Console.WriteLine("********************************");
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Enter the value of N :");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n\n");
+
+            for (count = 1; count <= num; count++)
+            {
+                Console.WriteLine("1/{0} +", count);
+                sum += 1 / (float)count;
+            }
+            Console.WriteLine("\n sum of series of {0} terms : {1} \n", num, sum);
+            Console.ReadLine();
+        }
+    }
+}
